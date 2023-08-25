e496fe365af55ffc0eaab5a1a982cd86a61d6de8
AlfredPaguio <alfredpaguio36@gmail.com>
2023-08-25 18:59:36 +0800

    Finished the tutorial and added bootstrap v5.3
    also added git ignore for code igniter 4
    
    I also purposely not ignoring .env because it's just a practice

---
+++ app/Views/news/create.php
@@ -0,0 +1,19 @@
+<h2><?= esc($title) ?></h2>
+
+<?= session()->getFlashdata('error') ?>
+<?= validation_list_errors() ?>
+
+<form action="/news/create" method="post">
+    <?= csrf_field() ?>
+
+    <label for="title" class="form-label">Title</label>
+    <input type="input" class="form-control" name="title" value="<?= set_value('title') ?>">
+    <br>
+
+    <label for="body" class="form-label">Text</label>
+    <textarea name="body" class="form-control" cols="45" rows="4"><?= set_value('body') ?></textarea>
+    <br>
+
+   
+    <input type="submit" class="btn btn-primary" name="submit" value="Create news item">
+</form>
---
+++ app/Views/news/view.php
@@ -0,0 +1,8 @@
+<div class="card">
+  <div class="card-header">
+  <?= esc($news['title']) ?>
+  </div>
+  <div class="card-body">
+    <p><?= esc($news['body']) ?></p>
+  </div>
+</div>
