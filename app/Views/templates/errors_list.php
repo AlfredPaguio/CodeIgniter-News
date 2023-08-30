ac1409e48242d620c4dc9b1c681b2cdf653e75c6
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2023-08-31 01:06:33 +0800

    Adjust error view and add missing home controller

--- app/Views/templates/errors_list.php
+++ app/Views/templates/errors_list.php
@@ -1,10 +1,13 @@
 <?php if (! empty($errors)): ?>
-    <div class="alert alert-danger d-flex align-items-center alert-dismissible fade show" role="alert">
-        <ul>
-        <?php foreach ($errors as $error): ?>
-            <li><?= esc($error) ?></li>
-        <?php endforeach ?>
-        </ul>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <div class="d-flex align-items-center">
+            <i class="fas fa-exclamation-triangle me-2"></i>
+            <ul class="mb-0">
+            <?php foreach ($errors as $error): ?>
+                <li><?= esc($error) ?></li>
+            <?php endforeach ?>
+            </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     </div>
 <?php endif ?>
